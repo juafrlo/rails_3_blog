abe9057397f5891e58478a41970d682e4118ab63
juafrlo <juafrlo@gmail.com>
2011-03-08 16:39:44 +0100

    Capture configuration exception

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,2 +1,6 @@
 #APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")[RAILS_ENV]
-APP_CONFIG = AppConfiguration.find_by_environment(ENV["RAILS_ENV"]).hashify
+begin
+  APP_CONFIG = AppConfiguration.find_by_environment(ENV["RAILS_ENV"]).hashify
+rescue
+  puts 'No configuration loaded'
+end
