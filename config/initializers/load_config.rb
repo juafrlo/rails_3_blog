70d4cc3e75d8b527e7fabe0212786216fc1cf4fa
juafrlo <juafrlo@gmail.com>
2011-03-08 16:16:55 +0100

    Load configuration

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,3 +1,2 @@
 #APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")[RAILS_ENV]
-APP_CONFIG = []
-#AppConfiguration.find_by_environment(ENV["RAILS_ENV"]).hashify
+APP_CONFIG = AppConfiguration.find_by_environment(ENV["RAILS_ENV"]).hashify
