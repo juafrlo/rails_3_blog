153c0ebac66bb227105cf86df6860b359182e857
juafrlo <juafrlo@gmail.com>
2011-03-08 18:47:01 +0100

    Updated contact text and small fixes

--- app/models/app_configuration.rb
+++ app/models/app_configuration.rb
@@ -2,11 +2,11 @@ class AppConfiguration < ActiveRecord::Base
   
   def hashify
     {
-      :environemnt => environment,
-      :user => user,
-      :password => password,
-      :email => email,
-      :site_url => site_url
+      'environemnt' => environment,
+      'user' => user,
+      'password' => password,
+      'email' => email,
+      'site_url' => site_url
     }
   end
 end
